999b01c8e899c436c4d2718c27dd4ed79ff8b749
Ethan <ebuntario@gmail.com>
2016-08-31 00:30:29 +0700

    Training

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,11 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ var name = "Ethan Buntario";
+ var formattedName = HTMLheaderName.replace("%data%", name);
+
+ var role = "UI Designer";
+ var formattedRole = HTMLheaderRole.replace("%data%", role);
+ 
+ $("#header").append(formattedRole);
+ $("#header").prepend(formattedName);
